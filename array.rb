b8e0866bf6de1852a0aa590776eac321e7e17fca
Yamamori <moriyama14t@gmail.com>
2023-02-28 22:05:12 +0900

    array追加

--- array.rb
+++ array.rb
@@ -1,4 +1,5 @@
 # 【参考】https://qiita.com/jnchito/items/118cca7ac2f01e1ca6a0
+#       https://qiita.com/yyykms123/items/e1bcb838f56100d12025
 
 p 'compact/compact!(nilの要素を取り除いた配列を新しく生成)'
 a = [1, nil, 'abc', false]
@@ -35,3 +36,39 @@ names = ['Alice', 'Bob', 'Carol']
 points = [80, 90, 60]
 # 名前と点数を組み合わせる
 p names.zip(points) #=> [["Alice", 80], ["Bob", 90], ["Carol", 60]]
+
+p 'mapメソッドは配列の各要素へ処理を行い、結果を新しい配列にして返す。'
+numbers = [1, 2, 3, 4, 5]
+new_numbers = []
+# 小なり２つ（<<）の意味 pushと同じ　https://prograshi.com/framework/rails/two-less-than-is-push/
+# メモ「arr << 6 << 8 << 10　」も可能
+numbers.map { |n| new_numbers << n * 10 }
+
+p new_numbers  #=> [10, 20, 30, 40, 50]
+
+p 'selectメソッドはブロックの戻り値が真（true）の要素を返し、新しい配列を作成'
+numbers = [1, 2, 3, 4, 5, 6]
+# ブロックの戻り値が真になった要素だけが集められる
+even_numbers = numbers.select { |n| n.even? }
+
+p even_numbers  #=> [2, 4, 6]
+
+p 'rejectメソッドはselectメソッドの反対で、ブロックの戻り値が偽（false）の要素を返し、新しい配列を作成'
+numbers = [1, 2, 3, 4, 5, 6]
+# 3の倍数を除外する（3の倍数以外を集める）
+non_multiples_of_three = numbers.reject { |n| n % 3 === 0 }
+p non_multiples_of_three  #=> [1, 2, 4, 5]
+
+p 'ブロックを簡潔に書く(&:)'
+# このコードは、
+['ruby', 'java', 'perl'].map { |s| s.upcase }
+#=> ['RUBY', 'JAVA', 'PERL']
+# こう書き換えられる
+['ruby', 'java', 'perl'].map(&:upcase)
+#=> ['RUBY', 'JAVA', 'PERL']
+# このコードは、
+[1, 2, 3, 4, 5, 6].select { |n| n.odd? }
+#=> [1, 3, 5]
+# こう書き換えられる
+[1, 2, 3, 4, 5, 6].select(&:odd?)
+#=> [1, 3, 5]
